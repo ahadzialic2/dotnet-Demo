a8ef9dd54d1c2c6055626643906a33170acd94ca
Adnan Hadzialic <ahadzialic2@etf.unsa.ba>
2022-05-26 09:33:10 +0200

    Asynchronous Calls

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using dotnet_rpg.Models;
 using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
@@ -23,18 +24,18 @@ namespace dotnet_rpg.Controllers
         //set-ali smo rutu GetAll za metodu ispod
         //public IActionResult Get() { //povratni tip IActionResult nam omogucava da saljemo specificne http kodove klijentu zajedno
         // sa konkretnim podacima koji su requestovani
-         public ActionResult<List<Character>> Get() { //promijenjen povratni tip u ActionResult -> da se prikaze json schema na swaggeru
-            return Ok(_characterService.GetAllCharacters()); //saljemo kod 200 i nas mock karakter 
+         public async Task<ActionResult<List<Character>>> Get() { //promijenjen povratni tip u ActionResult -> da se prikaze json schema na swaggeru
+            return Ok(await _characterService.GetAllCharacters()); //saljemo kod 200 i nas mock karakter 
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Character> GetSingle(int id) {
-            return Ok(_characterService.GetCharacterById(id)); //vrati default ako nema ni jedan da zadovoljana c.id == id(parametar)
+        public async Task<ActionResult<Character>> GetSingle(int id) {
+            return Ok(await _characterService.GetCharacterById(id)); //vrati default ako nema ni jedan da zadovoljana c.id == id(parametar)
         }
 
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character newCharacter) {
-            return Ok(_characterService.AddCharacter(newCharacter));
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter) {
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
     }
 }
