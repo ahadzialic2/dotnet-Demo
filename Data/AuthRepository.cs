4d79f8f563f832111b1bae4fbf00033969abdecf
Adnan Hadzialic <ahadzialic2@etf.unsa.ba>
2022-05-30 14:05:24 +0200

    auth repository

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace dotnet_Demo.Data
+﻿using System.Threading.Tasks;
+using dotnet_Demo.Models;
+
+namespace dotnet_Demo.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
