9d8c65f532deac67cd099c463dcfbbba62233b5d
Adnan Hadzialic <ahadzialic2@etf.unsa.ba>
2022-05-31 07:14:17 +0200

    Auth- User Login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -28,5 +28,18 @@ namespace dotnet_Demo.Controllers
             }
             return Ok(response);
         }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<int>>> Login(UserLoginDto request)
+        {
+            var response = await _authRepo.Login(
+                request.Username, request.Password
+                );
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
     }
 }
