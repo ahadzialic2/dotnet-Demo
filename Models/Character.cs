30a4c57d5002f0382de3fa9cf97a9fbffbc02ea1
Adnan Hadzialic <ahadzialic2@etf.unsa.ba>
2022-06-01 12:30:34 +0200

    One-To-One Relationship with Weapons

--- Models/Character.cs
+++ Models/Character.cs
@@ -13,5 +13,6 @@ namespace dotnet_Demo.Models
         public int Intelligence { get; set; } = 10;
         public RpgClass Class { get; set; } = RpgClass.Knight;
         public User User { get; set; }
+        public Weapon Weapon { get; set; }
     }
 }
